c7b657efbb7249a6e9fa0742c13aa6956e69ca50
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-10-11 16:37:12 +0700

    #6 [ Components Folder ] update lazy loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Suspense } from 'react';
 import './App.css';
 import RoutesPage from 'src/routers';
+import Loading from './components/Loading';
 
 function App() {
   return (
     <>
-      <RoutesPage />
+      <Suspense fallback={<Loading />}>
+        <RoutesPage />
+      </Suspense>
     </>
   );
 }
