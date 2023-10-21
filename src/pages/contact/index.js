9757d76abb55bdf447d7d4bf6148a842f8207c55
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-10-21 21:18:15 +0700

    #2 [ Pages Folder ] update learn_1

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,34 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
+import UseRef from './useRef/UseRef';
 
 const Contact = () => {
-  const [count, setCount] = useState(60);
-  const timeIdRef = useRef();
-  const prevCount = useRef();
-  const elementRef = useRef();
-  useEffect(() => {
-    prevCount.current = count;
-    console.info(prevCount.current);
-  }, [count]);
-  //
-  useEffect(() => {
-    const data = elementRef.current.getBoundingClientRect();
-    console.info(data);
-  }, [count]);
-  const handleCounter = () => {
-    timeIdRef.current = setInterval(() => {
-      setCount((pre) => pre - 1);
-    }, 1000);
-    console.info(timeIdRef.current, '--------1-----');
-  };
-  const hanldeStopCounter = () => {
-    clearInterval(timeIdRef.current);
-    console.info(timeIdRef.current, '--------2-----');
-  };
   return (
     <>
-      <h1 ref={elementRef}>{count}</h1>
-      <button onClick={handleCounter}>Start</button>
-      <button onClick={hanldeStopCounter}>Stop</button>
+      <UseRef />
     </>
   );
 };
---
+++ src/pages/contact/useRef/UseRef.js
@@ -0,0 +1,15 @@
+//* LIB
+import React from 'react';
+
+//* HOOK
+import AutoCount from './learn_1';
+
+const UseRef = () => {
+  return (
+    <>
+      <AutoCount />
+    </>
+  );
+};
+
+export default UseRef;
