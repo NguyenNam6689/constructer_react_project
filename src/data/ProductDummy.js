2426e398856a16950804c0281da4874a30ea57b9
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-10-24 21:56:07 +0700

    #12 [ Data Folder ] update data productdummy

---
+++ src/data/ProductDummy.js
@@ -0,0 +1,10 @@
+import { v4 as uuid } from 'uuid';
+export const productDummy = [
+  { id: uuid(), name: 'Product A', price: 20 },
+  { id: uuid(), name: 'Product B', price: 15 },
+  { id: uuid(), name: 'Product C', price: 25 },
+  { id: uuid(), name: 'Product D', price: 35 },
+  { id: uuid(), name: 'Product E', price: 105 },
+  { id: uuid(), name: 'Product F', price: 25 },
+  { id: uuid(), name: 'Product G', price: 90 },
+];
