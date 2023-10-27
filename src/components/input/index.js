f234e8d3a59a7450e725ceee4d6bb37a975a12cf
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-10-27 19:38:12 +0700

    #6 [ Components Folder ] update input

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  addTodoError,
+  addTodoPending,
+  addTodoSuccess,
+} from 'src/libs/Redux/react-redux/todos/Action';
 
 // CSS
 import 'src/styles/input.css';
 
 const InputTodo = () => {
   const [task, setTask] = useState('');
+  const dispatch = useDispatch();
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    if (task === '') return alert('Please write your task 🙃');
+    try {
+      dispatch(addTodoPending());
+      setTimeout(() => {
+        dispatch(addTodoSuccess(task));
+        alert('Add task successfully ');
+      }, 2000);
+    } catch (error) {
+      dispatch(addTodoError(error));
+    }
+  };
+
   return (
     <>
-      <form class="TodoInput">
+      <form className="TodoInput" onSubmit={handleAddTodo}>
         <input type="text" onChange={(e) => setTask(e.target.value)} />
       </form>
     </>
